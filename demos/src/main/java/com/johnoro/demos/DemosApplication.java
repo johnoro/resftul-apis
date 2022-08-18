f20cfe1254c3014d3e2061f7f78c7ecf36421802
johnoro <officialjohnorourke@gmail.com>
2022-08-18 15:56:06 -0400

    Adds swagger dependencies and updates boilerplate

--- demos/src/main/java/com/johnoro/demos/DemosApplication.java
+++ demos/src/main/java/com/johnoro/demos/DemosApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemosApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemosApplication.class, args);
 	}
-
 }
--- lorem/src/main/java/com/johnoro/lorem/LoremApplication.java
+++ lorem/src/main/java/com/johnoro/lorem/LoremApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LoremApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(LoremApplication.class, args);
 	}
-
 }
